16baf7d7e438fb05ba884ed427204653eebff129
Shen Zheng <shenz2@illinois.edu>
2022-12-05 21:12:18 -0600

    firebase added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,6 +5,8 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Alert from '@mui/material/Alert';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 
 function Login (props) {
     const { user, login, error } = useContext(UserContext);
@@ -14,8 +16,26 @@ function Login (props) {
 
     if (user && user.auth) return <Navigate to="/profile" />;
 
+    // The following lines are for firebase
+    
+    // Ends here
     const handleLogin = (e) => {
-        login(email, password);
+        const firebaseConfig = {
+            apiKey: "AIzaSyDK00wb-vgJbZbbZUrU7X2M08rjoNjQDHg",
+            authDomain: "final-25f43.firebaseapp.com",
+            projectId: "final-25f43",
+            storageBucket: "final-25f43.appspot.com",
+            messagingSenderId: "765153436621",
+            appId: "1:765153436621:web:45ee3e0ad3852d69481a84"
+        };
+        const app = initializeApp(firebaseConfig);
+        const auth = getAuth(app);
+        signInWithEmailAndPassword(auth, email, password).catch((error) => {
+            setStatus('error');
+            console.log("wrong pw");
+        });
+        // console.log(status)
+        if (status != "error") login(email, password);
         if (error) setStatus('error');
     }
 
